2b27dd5a0b0b9a90e46ae63df45c52cde3d64f4c
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-05-17 02:26:02 +0300

    assigned the response.body to the w struct

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
@@ -13,9 +13,25 @@ func main() {
 	api_key := os.Getenv("OPENWEATHER_API_KEY")
 	city := "cairo"
 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%v&appid=%v", city, api_key)
+
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	io.Copy(os.Stdout, resp.Body)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(resp.Status)
+	}
+
+	w := Weather{}
+	decoder := json.NewDecoder(resp.Body)
+	// decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(&w); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%v", w.Coord)
 }
